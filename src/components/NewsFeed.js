da424a775962313f93ca2b53c63a5e3fd7c1cb54
CarinaWollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2020-07-03 11:28:10 +0200

    added header

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Page from './Page';
  
 export class NewsFeed extends React.Component {
-    
+
     render() {
-        return <h1>Hello, World!</h1>
+        return (
+            <Page>
+                <h1>Hello, World!</h1>
+            </Page>
+            
+        );
     }
 }
 export default NewsFeed;
