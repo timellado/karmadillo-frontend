7a8b5eec867e917d09193f305d453570b1f791b5
CarinaWollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2020-07-03 11:18:44 +0200

    create empty NewsFeedView

---
+++ src/components/NewsFeed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+export class NewsFeed extends React.Component {
+    
+    render() {
+        return <h1>Hello, World!</h1>
+    }
+}
+export default NewsFeed;
+
+
+ 
---
+++ src/views/NewsFeedView.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import React from 'react';
+
+import { NewsFeed } from '../components/NewsFeed';
+
+
+
+export class NewsFeedView extends React.Component {
+    render() {
+        return (
+            <NewsFeed/>
+        );
+    }
+}
