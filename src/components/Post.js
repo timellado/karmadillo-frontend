8f49736b3ce54cb9daa6220116aab99426353a1f
CarinaWollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2020-07-03 15:05:20 +0200

    listing posts in basic design (mostly static data))

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import { SimpleLink } from './SimpleLink';
 
 import UserService from '../services/UserService';
-
+import '../css/images/App.css';
 
 export class Post extends React.Component {
 
@@ -17,7 +17,32 @@ export class Post extends React.Component {
 
     render() {
         return (
-                <div key={this.props.key}>{this.props.post.description}</div>
+            
+            <div className="post">
+                <div className="user">
+                        <div>Carina Wollendorfer</div>
+                </div>
+            <div className="created-at">
+                        20.7.2020
+                    </div>
+            <div key={this.props.key}>{this.props.post.description}</div>            
+            <div className="actions">
+              <div>
+                Like
+              </div>
+              <div>
+                Comment
+              </div>
+            </div>
+        </div>
+
+
+
+
+     /*   
+        <div className="test">
+            <div key={this.props.key}>{this.props.post.description}</div>
+        </div>*/
         );
     }
 }
