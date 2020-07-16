b316d7f7cdb88d6872aa5ea07420a64415ab74a5
CarinaWollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2020-07-16 21:52:01 +0200

    added checkbox

--- src/components/ActivityForm.js
+++ src/components/ActivityForm.js
@@ -8,21 +8,20 @@ import { AlertMessage } from './AlertMessage';
 import Page from './Page';
 
 
-const style = { maxWidth: 500 };
-
-
 class ActivityForm extends React.Component {
 
     constructor(props) {
         super(props);
             this.state = {
                 name : '',
-                category : ''
+                category : '', 
+                checked: false
             };
 
         this.handleChangeName = this.handleChangeName.bind(this);
         this.handleChangeCategory = this.handleChangeCategory.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleChangeName(value) {
@@ -42,42 +41,59 @@ class ActivityForm extends React.Component {
         this.props.onSubmit(activity);
     }
 
+    handleChange() {
+        this.setState({
+            checked: !this.state.checked
+        });
+
+    }
+    
     render() {
-
-
         return (
+
             <Page>
                 <div className="activity">
-
                     <div className="headline">New Activity</div>
-                    <form className="md-grid" onSubmit={this.handleSubmit} onReset={() => this.props.history.goBack()}>
-                        <TextField
-                            label="Activity Name*"
-                            id="NameField"
-                            type="text"
-                            className="md-row"
-                            required={true}
-                            value={this.state.name}
-                            onChange={this.handleChangeName}
-                            errorText=""/>
-                        <TextField
-                            label="Category *"
-                            id="CategoryField"
-                            type="text"
-                            className="md-row"
-                            required={true}
-                            value={this.state.category}
-                            onChange={this.handleChangeCategory}
-                            errorText=""/>
-                        <div className="buttons">
-                        <Button id="submit" type="submit"
-                                disabled={this.state.name == undefined || this.state.name == '' || this.state.category == undefined || this.state.category == ''}
-                                raised primary className="md-cell md-cell--2">Save</Button>
-                        <Button id="reset" type="reset" raised secondary className="md-cell md-cell--2">Dismiss</Button>
-                        <AlertMessage className="md-row md-full-width" >{this.props.error ? `${this.props.error}` : ''}</AlertMessage>
-                        </div>
-                    </form>
-                    </div>
+                        <form className="md-grid" onSubmit={this.handleSubmit} onReset={() => this.props.history.goBack()}>
+                        
+                            <TextField
+                                label="Activity Name*"
+                                id="NameField"
+                                type="text"
+                                className="md-row"
+                                required={true}
+                                value={this.state.name}
+                                onChange={this.handleChangeName}
+                                errorText=""/>
+                            <TextField
+                                label="Category *"
+                                id="CategoryField"
+                                type="text"
+                                className="md-row"
+                                required={true}
+                                value={this.state.category}
+                                onChange={this.handleChangeCategory}
+                                errorText=""/>   
+
+                            <div style={{ width: "100%" }}>
+                                <input type="checkbox" checked={ this.state.checked } onChange={ this.handleChange }></input>
+                                <label>I want to post this activity so my followers can see it</label>
+                            </div>
+
+                            {this.state.checked === true &&
+                            <div>if true show</div>
+                            }
+                                  
+                            <div className="buttons">
+                                <Button id="submit" type="submit"
+                                        disabled={this.state.name == undefined || this.state.name == '' || this.state.category == undefined || this.state.category == ''}
+                                        raised primary className="md-cell md-cell--2">Save
+                                </Button>
+                                <Button id="reset" type="reset" raised secondary className="md-cell md-cell--2">Dismiss</Button>
+                                <AlertMessage className="md-row md-full-width" >{this.props.error ? `${this.props.error}` : ''}</AlertMessage>
+                            </div>
+                        </form>
+                </div>
             </Page>
         );
     }
