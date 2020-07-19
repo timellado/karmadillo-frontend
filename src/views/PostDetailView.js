27cf092f799a76bf6989b1f645fea6b4162a9b90
timellado <timellado@github.com>
2020-07-19 20:35:35 +0200

    comment creation working

--- src/views/PostDetailView.js
+++ src/views/PostDetailView.js
@@ -5,6 +5,8 @@ import React from 'react';
 import { PostDetail } from '../components/PostDetail';
 
 import PostService from '../services/PostService';
+import UserService from '../services/UserService';
+import CommentService from '../services/CommentService';
 
 
 export class PostDetailView extends React.Component {
@@ -23,8 +25,17 @@ export class PostDetailView extends React.Component {
         (async () => {
             try {
                 let post = await PostService.getPost(id);
+                let postCreator = await UserService.getUser(post.activity.user);
+                let postComments = await CommentService.getCommentFromPost(id);
+                for (const comment of postComments) {
+                    let commentUser = await UserService.getUser(comment.user);
+                    comment["commentUser"] = commentUser.username;
+                }
+                console.log(postComments);
                 this.setState({
                     post: post,
+                    postCreator: postCreator,
+                    comments: postComments,
                     loading: false
                 });
             } catch(err) {
@@ -51,13 +62,25 @@ export class PostDetailView extends React.Component {
         }
     }
 
+    async createComment(comment) {
+        try {
+            let postComment = await CommentService.createComment(comment);
+            console.log(postComment);
+            this.props.history.go(0);
+        } catch(err) {
+            console.error(err);
+            this.setState(Object.assign({}, this.state, {error: 'Error while creating comment'}));
+        }
+        
+    }
+
     render() {
         if (this.state.loading) {
             return (<h2>Loading...</h2>);
         }
 
         return (
-            <PostDetail post={this.state.post} onDelete={(id) => this.deletePost(id)}/>
+            <PostDetail comments={this.state.comments} postCreator={this.state.postCreator} post={this.state.post} onDelete={(id) => this.deletePost(id)} onSubmit={(comment) => this.createComment(comment)}/>
         );
     }
 }
